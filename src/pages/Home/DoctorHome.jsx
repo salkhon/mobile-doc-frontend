862fa77aae27e3443ad34de00785ed1c915f717b
Haz Sameen Shahgir <83033987+Patchwork53@users.noreply.github.com>
2023-09-08 17:07:41 +0000

    doc dash done

--- src/pages/Home/DoctorHome.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function PatientHome() {
-	return <h2>I am doctor</h2>;
-}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,30 +1,22 @@
 import React from "react";
-import { Grid } from "@mui/material";
 import { useAuth } from "../../hooks/auth";
-import Header from "../../components/Header/Header";
-import DoctorHome from "./DoctorHome";
-import PatientHome from "./PatientHome";
-
+import DoctorDash from "./DoctorDash/main";
+import PatientDash from "./PatientDash/main";
 export function Home() {
 	const { userName, userType } = useAuth();
 
-	return (
-		<Grid
-			container
-			justifyContent="space-between"
-			alignItems="center"
-			margin="20px 0 20px 20px"
-			width="97.7%"
-		>
-			<Grid item xs={12}>
-				<Header
-					title="Home"
-					subtitle={`Welcome, ${userType} ${userName}`}
-				/>
-			</Grid>
-			{userType === "doctor" ? <DoctorHome /> : <PatientHome />}
-		</Grid>
-	);
+	if (userType === "doctor") {
+		return (
+			<DoctorDash />
+		);
+	}
+
+	if (userType === "patient") {
+		return (
+			<PatientDash />
+		);
+	}
+
 }
 
 export default Home;
