368e2c09c77339b6d79f91f11f9ae4332f5ee851
salkhon <salmankhondker1998@gmail.com>
2023-09-11 14:15:29 +0600

    sized analytics

--- src/pages/Analytics/index.jsx
+++ src/pages/Analytics/index.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 export default function Analytics() {
 	return (
-		<Box height="127vh" width="83vw" m={3}>
+		<Box height="88%" width="94%" m={3}>
 			<iframe
 				title="abc"
-				width="100%"
-				height="100%"
+				width="70%"
+				height="97%"
 				src="https://lookerstudio.google.com/embed/reporting/7d72beb9-040f-491a-91fa-d806b6adaa3a/page/p_7lwjy3r88c"
 				style={{
 					border: 0,
